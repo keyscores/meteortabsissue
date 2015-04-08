51a6f7ccfd2fd9c8cc73be647cd1ff1f5213f3a5
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2015-04-08 13:28:33 -0400

    revisions

--- tabissue.js
+++ tabissue.js
@@ -3,12 +3,9 @@ if (Meteor.isClient) {
 
 Template.layout.rendered = function(){
  	 $('ul.tabs').tabs()
-	}
-}
+	},
 
-//The only way to do it is to have each page render, which is not effective if there are N pages.
-Template.layout.rendered = function(){
+Template.notIdeal.rendered = function(){
  	 $('ul.tabs').tabs()
 	}
 }
-
