f36c8f99e394d0a3faf3a40728e1464a3849f7ae
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2015-04-08 16:00:16 -0400

    clarity

--- tabissue.js
+++ tabissue.js
@@ -1,11 +1,15 @@
 if (Meteor.isClient) {
 
 
-Template.layout.rendered = function(){
- 	 $('ul.tabs').tabs()
-	},
+	Template.layout.rendered = function(){
+		 $('ul.tabs').tabs()
+		},
+		
+	Template.body.rendered = function(){
+		 $('ul.tabs').tabs()
+		},
 
-Template.notIdeal.rendered = function(){
- 	 $('ul.tabs').tabs()
-	}
+	Template.notIdeal.rendered = function(){
+		 $('ul.tabs').tabs()
+		}
 }
